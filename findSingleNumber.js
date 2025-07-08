817cb753877ac4a2d6e3c8eafeaf210d1ebd768f
vengalreddy2000 <vengalareddy@theretailinsights.com>
2025-07-08 22:55:52 +0530

    find single didgit code added

--- findSingleNumber.js
+++ findSingleNumber.js
@@ -1,9 +1,29 @@
-let nums=[4, 1, 2, 1, 2]
-function findSingleNumber(nums){
-    let xor=0
-    for(let i=0;i<nums.length;i++){
-        xor=xor ^ nums[i]
+let nums = [4, 1, 2, 1, 2];
+
+// Approach 1
+function findSingleNumber(nums) {
+  let hash = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (!hash[nums[i]]) {
+      hash[nums[i]] = 1;
+    } else {
+      hash[nums[i]]++;
     }
-    return xor
+  }
+  for (let i = 0; i < nums.length; i++) {
+    if (hash[nums[i]]===1) {
+      return nums[i];
+    } 
+  }
+ 
 }
-console.log(findSingleNumber(nums))
+
+// Approach 2
+function findSingleNumber(nums) {
+  let xor = 0;
+  for (let i = 0; i < nums.length; i++) {
+    xor = xor ^ nums[i];
+  }
+  return xor;
+}
+console.log(findSingleNumber(nums));
