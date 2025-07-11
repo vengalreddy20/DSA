e910fee9103318c7f5bc24baf7e6af1e94a9ef6b
vengalreddy2000 <vengalareddy@theretailinsights.com>
2025-07-11 08:13:23 +0530

    bubble sort

---
+++ SearchingAndSorting/bubbleSort.js
@@ -0,0 +1,16 @@
+let arr = [5, 0, 1, 2, 7, 9, 3, 4];
+
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let isSwapped = false;
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        isSwapped = true;
+      }
+    }
+    if (!isSwapped) break;
+  }
+  return arr;
+}
+console.log(bubbleSort(arr));
